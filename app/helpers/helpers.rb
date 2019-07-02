6f58f94517ac8448225a5ef41a798b829078c940
M Banks <montensia1@outlook.com>
2019-07-02 21:03:42 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    
+    #need the exclamations to return true or false instead of returning a value
+    #Does session have a :user_id?
+    !!session[:user_id]
+  end
 end
