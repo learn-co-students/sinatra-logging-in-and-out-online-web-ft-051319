f7482a19e903454a026fbd72bc54a294c767288e
Natalie <natsetc@gmail.com>
2019-07-06 14:03:57 -0700

    add notes

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,4 +1,5 @@
-class Helpers < Sinatra::Base
+class Helpers 
+    # < Sinatra::Base
     def self.current_user(session)
         @user = User.find(session[:user_id])
         @user
@@ -6,5 +7,9 @@ class Helpers < Sinatra::Base
 
     def self.is_logged_in?(session)
         !!session[:user_id] ? true : false
+        # double bang will convert value to a boolean
+        # without double bang, value will be nil
+        # !!session[:user_id] 
+        # dont need ternary operator
     end
 end
