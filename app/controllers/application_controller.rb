7f124a16298139eef76da28b3bd76ef857485ac7
Alethea Cary <aletheacary@gmail.com>
2019-07-19 05:47:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     if session[:user_id]
-      @user = User.find_by(session[:user_id])
+      @user = User.find_by(session[:user_id].to_s)
       erb :account
     else
       erb :error
---
+++ db/migrate/20190719054148_change_balance_to_int.rb
@@ -0,0 +1,5 @@
+class ChangeBalanceToInt < ActiveRecord::Migration[5.1]
+  def change
+    change_column :users, :balance, :integer
+  end
+end
